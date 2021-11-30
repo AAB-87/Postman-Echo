b8ebc720ea4e5871e64f402e353d0ec167b04da2
AAB-87 <andrej-abrekov@yandex.ru>
2021-11-30 10:14:25 +0400

    fixed PostmanEchoTest

--- src/test/java/ru/netology/test/PostmanEchoTest.java
+++ src/test/java/ru/netology/test/PostmanEchoTest.java
@@ -1,8 +1,9 @@
 package ru.netology.test;
 
-import io.restassured.http.ContentType;
+
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEchoTest {
     @Test
@@ -18,7 +19,7 @@ public class PostmanEchoTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body(/* --> ваша проверка здесь <-- */)
+                .body("data", equalTo("some data"))
         ;
     }
 }
